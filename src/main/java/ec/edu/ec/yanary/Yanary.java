06042a748312c182cf4a41b8fa7cf49efd983724
welinton-cool <welinton.sacon@espoch.edu.ec>
2024-11-22 17:43:45 -0500

    hola

---
+++ src/main/java/ec/edu/ec/yanary/Yanary.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ec.yanary;
+
+/**
+ *
+ * @author HP
+ */
+public class Yanary {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
